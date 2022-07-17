1ae137406682da9888873a2d2866d5ad675b78fc
DamiaoCastro <damiao.c@hotmail.com>
2022-07-17 06:52:19 +0200

    comments are working

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 #### docs
 https://developer.mozilla.org/en-US/docs/WebAssembly/Rust_to_wasm
 https://code.visualstudio.com/docs/cpp/config-wsl#_set-up-your-linux-environment
+https://docs.rs/regex/latest/regex/
 
 #### build the package
  wasm-pack build --target nodejs
--- src/bqsql_document_parser.rs
+++ src/bqsql_document_parser.rs
@@ -10,7 +10,7 @@ impl BqsqlDocument {
 
         let mut items = Vec::new();
 
-        if let Some(comment) = handle_comment(bqsql, position) {
+        if let Some(comment) = handle_comment(bqsql, &position) {
             items.push(comment);
         }
 
@@ -21,19 +21,18 @@ impl BqsqlDocument {
     }
 }
 
-fn handle_comment(bqsql: &str, position: BqsqlDocumentPosition) -> Option<BqsqlDocumentItem> {
+fn handle_comment(bqsql: &str, position: &BqsqlDocumentPosition) -> Option<BqsqlDocumentItem> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"(\s)?--.").unwrap();
+        static ref RE: Regex = Regex::new(r"^(\s*)--.*").unwrap();
     }
     if let Some(comment_match) = RE.find_at(bqsql, position.index) {
         // print!(comment_match.);
-        let _pos2 = comment_match.range();
-        let _pos1 = comment_match.start();
+        let end = comment_match.end();
 
         return Some(BqsqlDocumentItem {
             item_type: BqsqlDocumentItemType::COMMENT,
-            from: BqsqlDocumentPosition::beginning_text(),
-            to: BqsqlDocumentPosition::beginning_text(),
+            from: BqsqlDocumentPosition::move_to_non_white(bqsql, position),
+            to: BqsqlDocumentPosition::move_end(position, end),
         });
     }
     None
@@ -61,36 +60,39 @@ mod tests {
         assert_eq!(0, document.items[0].from.column);
         assert_eq!(0, document.items[0].from.line);
         assert_eq!(0, document.items[0].from.index);
-        // assert_eq!(15, document.items[0].to.column);
-        // assert_eq!(0, document.items[0].to.line);
-        // assert_eq!(15, document.items[0].to.index);
+        assert_eq!(15, document.items[0].to.column);
+        assert_eq!(0, document.items[0].to.line);
+        assert_eq!(15, document.items[0].to.index);
     }
 
     #[test]
     fn space_comment_only() {
-        let document = BqsqlDocument::parse("   --super comment");
+        let document = BqsqlDocument::parse("    --super comment");
 
         assert_eq!(BqsqlDocumentType::UNKNOWN, document.document_type);
         assert_eq!(1, document.items.len());
         assert_eq!(BqsqlDocumentItemType::COMMENT, document.items[0].item_type);
-        // assert_eq!(4, document.items[0].from.column);
-        // assert_eq!(1, document.items[0].from.line);
-        // assert_eq!(4, document.items[0].from.index);
-        // assert_eq!(19, document.items[0].to.column);
-        // assert_eq!(1, document.items[0].to.line);
-        // assert_eq!(19, document.items[0].to.index);
+        assert_eq!(4, document.items[0].from.column);
+        assert_eq!(0, document.items[0].from.line);
+        assert_eq!(4, document.items[0].from.index);
+        assert_eq!(19, document.items[0].to.column);
+        assert_eq!(0, document.items[0].to.line);
+        assert_eq!(19, document.items[0].to.index);
     }
 
     #[test]
-    #[ignore]
     fn comment_and_tiny_query() {
         let document = BqsqlDocument::parse("--super comment\nSELECT 2+2");
 
-        assert_eq!(BqsqlDocumentType::QUERY, document.document_type);
-        assert_eq!(2, document.items.len());
+        // assert_eq!(BqsqlDocumentType::QUERY, document.document_type);
+        // assert_eq!(2, document.items.len());
         assert_eq!(BqsqlDocumentItemType::COMMENT, document.items[0].item_type);
         assert_eq!(0, document.items[0].from.column);
         assert_eq!(0, document.items[0].from.line);
         assert_eq!(0, document.items[0].from.index);
+        assert_eq!(15, document.items[0].to.column);
+        assert_eq!(0, document.items[0].to.line);
+        assert_eq!(15, document.items[0].to.index);
+
     }
 }

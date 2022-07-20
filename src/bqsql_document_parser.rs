55d4c1b49c886d079bba337a595987a52ed85430
DamiaoCastro <damiao.c@hotmail.com>
2022-07-20 21:55:26 +0200

    nothing good

--- src/bqsql_document_parser.rs
+++ src/bqsql_document_parser.rs
@@ -43,27 +43,47 @@ impl BqsqlDocument {
 
 fn parse_tokens(bqsql: &str) -> Vec<BqsqlDocumentToken> {
     lazy_static! {
-        static ref RE: Regex = Regex::new("(--.*$)|(\\d?\\.\\d?)|\\b").unwrap();
+        static ref RE: Regex = Regex::new("\\b|\\d?\\.\\d?").unwrap();
+        static ref regex_comment: Regex = Regex::new("--.*").unwrap();
 
+        //|\\d?\\.\\d?
         //|(\".+\")
+        //(--.*$)|
     }
 
     let mut tokens: Vec<BqsqlDocumentToken> = Vec::new();
 
     let mut line_index: usize = 0;
     for line in bqsql.lines() {
+        
+        let mut adjusted_line: &str = line;
+        let mut line_comment: Option<BqsqlDocumentToken> = None;
+
+        //remove line comments out of the match.
+        // to simplify the process
+        //putting top many conditions on the regex was becomming difficult to control
+        if let Some(m) = regex_comment.find(line) {
+            adjusted_line = &line[0..m.start()];
+
+            line_comment = Some(BqsqlDocumentToken {
+                from: BqsqlDocumentPosition::new(line_index, m.start()),
+                to: BqsqlDocumentPosition::new(line_index, line.len()),
+                token: line[m.start()..].to_string(),
+            });
+        }
+
         let mut split1 = RE
-            .find_iter(line)
+            .find_iter(adjusted_line)
             .map(|m| m.start())
             .collect::<Vec<usize>>();
 
         let mut previous_start: Option<usize> = None;
 
-        split1.push(line.len());
+        split1.push(adjusted_line.len());
 
         for start in split1 {
             if let Some(previous) = previous_start {
-                let partial = line[previous..start].to_string();
+                let partial = adjusted_line[previous..start].to_string();
 
                 if partial.trim().len() > 0 {
                     tokens.push(BqsqlDocumentToken {
@@ -74,6 +94,10 @@ fn parse_tokens(bqsql: &str) -> Vec<BqsqlDocumentToken> {
                 }
             }
             previous_start = Some(start);
+        }
+
+        if line_comment.is_some() {
+            tokens.push(line_comment.unwrap());
         }
 
         line_index = line_index + 1;
@@ -92,36 +116,40 @@ fn parse_tokens_single_line_operation() {
     assert_eq!(4, result[0].from.character);
     assert_eq!(0, result[0].to.line);
     assert_eq!(10, result[0].to.character);
-    
+
     assert_eq!("23", result[1].token);
     assert_eq!(0, result[1].from.line);
     assert_eq!(11, result[1].from.character);
     assert_eq!(0, result[1].to.line);
     assert_eq!(13, result[1].to.character);
-    
+
     assert_eq!("+", result[2].token);
     assert_eq!(0, result[2].from.line);
     assert_eq!(13, result[2].from.character);
     assert_eq!(0, result[2].to.line);
     assert_eq!(14, result[2].to.character);
-    
+
     assert_eq!("2.45", result[3].token);
     assert_eq!(0, result[3].from.line);
     assert_eq!(14, result[3].from.character);
     assert_eq!(0, result[3].to.line);
     assert_eq!(18, result[3].to.character);
-    
-    assert_eq!("--test, another `table` 123 \"back\" to 'dust'", result[4].token);
+
+    assert_eq!(
+        "--test, another `table` 123 \"back\" to 'dust'",
+        result[4].token
+    );
     assert_eq!(0, result[4].from.line);
     assert_eq!(19, result[4].from.character);
     assert_eq!(0, result[4].to.line);
     assert_eq!(63, result[4].to.character);
-    
 }
 
 #[test]
 fn parse_tokens_single_line_string() {
-    let result = parse_tokens("SELECT \"this is a ''' string \"--test, another `table` 123 \"back\" to 'dust'");
+    let result = parse_tokens(
+        "SELECT \"this is a ''' string \" --test, another `table` 123 \"back\" to 'dust'",
+    );
 
     assert_eq!(3, result.len());
     assert_eq!("SELECT", result[0].token);
@@ -129,32 +157,46 @@ fn parse_tokens_single_line_string() {
     assert_eq!(0, result[0].from.character);
     assert_eq!(0, result[0].to.line);
     assert_eq!(6, result[0].to.character);
-    
-    // assert_eq!("23", result[1].token);
-    // assert_eq!(0, result[1].from.line);
-    // assert_eq!(11, result[1].from.character);
+
+    // assert_eq!("\"this is a ''' string \"", result[1].token);
+    assert_eq!(0, result[1].from.line);
+    assert_eq!(7, result[1].from.character);
     // assert_eq!(0, result[1].to.line);
-    // assert_eq!(13, result[1].to.character);
-    
-    // assert_eq!("+", result[2].token);
-    // assert_eq!(0, result[2].from.line);
-    // assert_eq!(13, result[2].from.character);
-    // assert_eq!(0, result[2].to.line);
-    // assert_eq!(14, result[2].to.character);
-    
-    // assert_eq!("2.45", result[3].token);
-    // assert_eq!(0, result[3].from.line);
-    // assert_eq!(14, result[3].from.character);
-    // assert_eq!(0, result[3].to.line);
-    // assert_eq!(18, result[3].to.character);
-    
-    // assert_eq!("--test, another `table` 123 \"back\" to 'dust'", result[4].token);
-    // assert_eq!(0, result[4].from.line);
-    // assert_eq!(19, result[4].from.character);
-    // assert_eq!(0, result[4].to.line);
-    // assert_eq!(63, result[4].to.character);
-    
+    // assert_eq!(30, result[1].to.character);
+
+    assert_eq!("--test, another `table` 123 \"back\" to 'dust'", result[2].token);
+    assert_eq!(0, result[2].from.line);
+    assert_eq!(31, result[2].from.character);
+    assert_eq!(0, result[2].to.line);
+    assert_eq!(75, result[2].to.character);
+
 }
+
+
+#[test]
+fn parse_tokens_single_line_string_with_double_dash() {
+    let result = parse_tokens(
+        "SELECT \"this is a -- string \"  ",
+    );
+
+    assert_eq!(2, result.len());
+    assert_eq!("SELECT", result[0].token);
+    assert_eq!(0, result[0].from.line);
+    assert_eq!(0, result[0].from.character);
+    assert_eq!(0, result[0].to.line);
+    assert_eq!(6, result[0].to.character);
+
+    assert_eq!("\"this is a -- string \"", result[1].token);
+    assert_eq!(0, result[1].from.line);
+    assert_eq!(7, result[1].from.character);
+    // assert_eq!(0, result[1].to.line);
+    // assert_eq!(30, result[1].to.character);
+
+}
+
+
+
+
 // fn handle_comment(bqsql: &str, position: &BqsqlDocumentPosition) -> Option<BqsqlDocumentItem> {
 //     lazy_static! {
 //         static ref RE: Regex = Regex::new(r"^\s*--.*").unwrap();

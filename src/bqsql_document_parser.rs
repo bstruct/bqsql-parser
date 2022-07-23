1f2165841e323f6950f1177f4e11a54211b8cfa7
DamiaoCastro <damiao.c@hotmail.com>
2022-07-23 07:06:45 +0200

    improvement

--- src/bqsql_document_parser.rs
+++ src/bqsql_document_parser.rs
@@ -43,12 +43,9 @@ impl BqsqlDocument {
 
 fn parse_tokens(bqsql: &str) -> Vec<BqsqlDocumentToken> {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"\d?\.\d?|\b").unwrap();
-        // static ref regex_comment: Regex = Regex::new("--.*").unwrap();
-
-        //|\\d?\\.\\d?
-        //|(\".+\")
-        //(--.*$)|
+        static ref RE: Regex = Regex::new(r"\w+|\W{1}").unwrap();
+        //^|\d+|\s+|\W{1}
+        //|\d*\.\d*
     }
 
     let mut tokens: Vec<BqsqlDocumentToken> = Vec::new();
@@ -80,31 +77,20 @@ fn parse_tokens(bqsql: &str) -> Vec<BqsqlDocumentToken> {
             }
 
             let adjusted_line = &line[gap[0]..gap[1]];
+            println!("adjusted_line len: {}", adjusted_line.len());
 
-            let split1 = RE
-                .find_iter(adjusted_line)
-                .map(|m| m.start())
-                .collect::<Vec<usize>>();
+            for m in RE.find_iter(adjusted_line) {
+                let partial = adjusted_line[m.start()..m.end()].to_string();
 
-            let mut previous_start: Option<usize> = None;
+                println!("partial: {}", partial);
 
-            // split1.push(adjusted_line.len());
-
-            for start in split1 {
-                if let Some(previous) = previous_start {
-                    let partial = adjusted_line[previous..start].to_string();
-
-                    // println!("partial: {}", partial);
-
-                    if partial.trim().len() > 0 {
-                        tokens.push(BqsqlDocumentToken {
-                            from: BqsqlDocumentPosition::new(line_index, previous),
-                            to: BqsqlDocumentPosition::new(line_index, start),
-                            token: partial,
-                        });
-                    }
+                if partial.trim().len() > 0 {
+                    tokens.push(BqsqlDocumentToken {
+                        from: BqsqlDocumentPosition::new(line_index, gap[0] + m.start()),
+                        to: BqsqlDocumentPosition::new(line_index, gap[0] + m.end()),
+                        token: partial,
+                    });
                 }
-                previous_start = Some(start);
             }
         }
 
@@ -185,6 +171,33 @@ fn parse_tokens_single_line_string() {
 }
 
 #[test]
+fn parse_tokens_parenthisis() {
+    let result = parse_tokens("SELECT (((1)))");
+
+    assert_eq!(8, result.len());
+    // assert_eq!("SELECT", result[0].token);
+    // assert_eq!(0, result[0].from.line);
+    // assert_eq!(0, result[0].from.character);
+    // assert_eq!(0, result[0].to.line);
+    // assert_eq!(6, result[0].to.character);
+
+    // assert_eq!("\"this is a ''' string \"", result[1].token);
+    // assert_eq!(0, result[1].from.line);
+    // assert_eq!(7, result[1].from.character);
+    // assert_eq!(0, result[1].to.line);
+    // assert_eq!(30, result[1].to.character);
+
+    // assert_eq!(
+    //     "--test, another `table` 123 \"back\" to 'dust'",
+    //     result[2].token
+    // );
+    // assert_eq!(0, result[2].from.line);
+    // assert_eq!(31, result[2].from.character);
+    // assert_eq!(0, result[2].to.line);
+    // assert_eq!(75, result[2].to.character);
+}
+
+#[test]
 fn parse_tokens_single_line_string_with_double_dash() {
     let result = parse_tokens("SELECT \"this is a -- string \"  ");
 
@@ -200,6 +213,68 @@ fn parse_tokens_single_line_string_with_double_dash() {
     assert_eq!(7, result[1].from.character);
     assert_eq!(0, result[1].to.line);
     assert_eq!(29, result[1].to.character);
+}
+
+#[test]
+fn parse_tokens_strings() {
+    let result =
+        parse_tokens(" SELECT 'this is a \\' -- string ',\"this is also a \\\" -- string \"");
+
+    assert_eq!(4, result.len());
+    assert_eq!("SELECT", result[0].token);
+    assert_eq!(0, result[0].from.line);
+    assert_eq!(1, result[0].from.character);
+    assert_eq!(0, result[0].to.line);
+    assert_eq!(7, result[0].to.character);
+
+    assert_eq!("'this is a \\' -- string '", result[1].token);
+    assert_eq!(0, result[1].from.line);
+    assert_eq!(8, result[1].from.character);
+    assert_eq!(0, result[1].to.line);
+    assert_eq!(33, result[1].to.character);
+
+    assert_eq!(",", result[2].token);
+    assert_eq!(0, result[2].from.line);
+    assert_eq!(33, result[2].from.character);
+    assert_eq!(0, result[2].to.line);
+    assert_eq!(34, result[2].to.character);
+
+    assert_eq!("\"this is also a \\\" -- string \"", result[3].token);
+    assert_eq!(0, result[3].from.line);
+    assert_eq!(34, result[3].from.character);
+    assert_eq!(0, result[3].to.line);
+    assert_eq!(64, result[3].to.character);
+}
+
+#[test]
+fn parse_tokens_strings_with_space() {
+    let result =
+        parse_tokens(" SELECT 'this is a \\' -- string ', \"this is also a \\\" -- string \"");
+
+    assert_eq!(4, result.len());
+    assert_eq!("SELECT", result[0].token);
+    assert_eq!(0, result[0].from.line);
+    assert_eq!(1, result[0].from.character);
+    assert_eq!(0, result[0].to.line);
+    assert_eq!(7, result[0].to.character);
+
+    assert_eq!("'this is a \\' -- string '", result[1].token);
+    assert_eq!(0, result[1].from.line);
+    assert_eq!(8, result[1].from.character);
+    assert_eq!(0, result[1].to.line);
+    assert_eq!(33, result[1].to.character);
+
+    assert_eq!(",", result[2].token);
+    assert_eq!(0, result[2].from.line);
+    assert_eq!(33, result[2].from.character);
+    assert_eq!(0, result[2].to.line);
+    assert_eq!(34, result[2].to.character);
+
+    assert_eq!("\"this is also a \\\" -- string \"", result[3].token);
+    assert_eq!(0, result[3].from.line);
+    assert_eq!(35, result[3].from.character);
+    assert_eq!(0, result[3].to.line);
+    assert_eq!(65, result[3].to.character);
 }
 
 fn find_strings_and_line_comments(line: &str) -> Vec<[usize; 2]> {
@@ -308,82 +383,10 @@ fn find_strings_and_line_comments_strings() {
         " SELECT 'this is a \\' -- string ',\"this is also a \\\" -- string \"",
     );
 
-    assert_eq!(4, result.len());
-    // assert_eq!(8, result[0][0]);
-    // assert_eq!(33, result[0][1]);
+    assert_eq!(2, result.len());
+    assert_eq!(8, result[0][0]);
+    assert_eq!(33, result[0][1]);
 
-    // assert_eq!(34, result[1][0]);
-    // assert_eq!(78, result[1][1]);
+    assert_eq!(34, result[1][0]);
+    assert_eq!(64, result[1][1]);
 }
-
-// fn handle_comment(bqsql: &str, position: &BqsqlDocumentPosition) -> Option<BqsqlDocumentItem> {
-//     lazy_static! {
-//         static ref RE: Regex = Regex::new(r"^\s*--.*").unwrap();
-//     }
-//     if let Some(comment_match) = RE.find_at(bqsql, position.index) {
-//         let end = comment_match.end();
-
-//         return Some(BqsqlDocumentItem {
-//             item_type: BqsqlDocumentItemType::COMMENT,
-//             from: BqsqlDocumentPosition::move_to_non_white(bqsql, position),
-//             to: BqsqlDocumentPosition::move_end(position, end - 1),
-//         });
-//     }
-//     None
-// }
-
-// fn handle_query(bqsql: &str, position: &BqsqlDocumentPosition) -> Option<BqsqlDocumentItem> {
-//     lazy_static! {
-//         static ref RE: Regex = Regex::new(r"^[\t\v\f\r ]*SELECT\s+").unwrap();
-//     }
-
-//     if let Some(select_match) = RE.find(&bqsql[position.index..]) {
-//         let end = select_match.end();
-
-//         //find the position of the word "SELECT"
-
-//         //const or simple calculation as type, example: "hi" AS name, or 1 AS number, or 2+2 as another_number
-//         //column with potential alias
-
-//         return Some(BqsqlDocumentItem {
-//             item_type: BqsqlDocumentItemType::QUERY,
-//             from: BqsqlDocumentPosition::move_to_non_white(bqsql, position),
-//             to: BqsqlDocumentPosition::move_end(position, end),
-//         });
-//     }
-//     None
-// }
-
-// #[test]
-// fn test_handle_query() {
-//     let result = handle_query("SELECT 2+2", &BqsqlDocumentPosition::beginning_text());
-//     assert!(result.is_some());
-// }
-
-// #[test]
-// fn test_handle_query_after_comment() {
-//     let result = handle_query(
-//         "--super comment\nSELECT 2+2",
-//         &BqsqlDocumentPosition {
-//             column: 0,
-//             line: 1,
-//             index: 16,
-//         },
-//     );
-//     assert!(result.is_some());
-// }
-
-// #[test]
-// fn test_handle_query_new_line() {
-//     let result = handle_query("\nSELECT 2+2", &BqsqlDocumentPosition::beginning_text());
-//     assert!(result.is_none());
-// }
-
-// #[test]
-// fn test_handle_query_new_line_space() {
-//     let result = handle_query("\n SELECT 2+2", &BqsqlDocumentPosition::beginning_text());
-//     assert!(result.is_none());
-// }
-
-// #[cfg(test)]
-// mod test_select;

26bc29cab0046df37c12af1cbda09288744458f8
DamiaoCastro <damiao.c@hotmail.com>
2022-08-06 21:48:16 +0200

    .

--- src/bqsql_document/bqsql_interpreter_query_full_test.rs
+++ src/bqsql_document/bqsql_interpreter_query_full_test.rs
@@ -9,3 +9,20 @@ fn queries_file() {
 
     assert_eq!(97, document.items.len());
 }
+
+#[test]
+#[ignore = "not ready yet"]
+fn queries_file_split() {
+    let bqsql = include_str!("query_files/queries.bqsql");
+
+    let split = bqsql.split(";");
+    for s in split {
+        let document = BqsqlDocument::parse(s);
+
+        if document.items.len() > 1 {
+            println!("{}", &s);
+        }
+
+        assert_eq!(1, document.items.len());
+    }
+}
